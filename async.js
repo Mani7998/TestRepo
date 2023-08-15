c5dcbce8791049b18c85e03351c37fc7a2373afb
Mani7998 <manikanta.pabbaraju@applaudhr.com>
2023-08-15 19:08:41 +0530

    Hello1

--- async.js
+++ async.js
@@ -1,7 +1,7 @@
 exports = async function() {
      // Supply projectID and clusterName...
     const projectID = '603ce6b799758b5d75cb40ea';
-    const clusterName = 'tdevelopment-xyzabcdefghi';
+    const clusterName = 'Hello1';
     
      // Get stored credentials...
     const username = context.values.get("AtlasPublicKey");
